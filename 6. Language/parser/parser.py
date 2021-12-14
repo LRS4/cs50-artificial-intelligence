5e189bb0ab6208bff46c7f07e22566a6e9ed699d
LRS4 <lrspencer@hotmail.co.uk>
2021-12-14 10:01:27 +0000

    First implementation of preprocess using nltk word_tokenise

--- 6. Language/parser/parser.py
+++ 6. Language/parser/parser.py
@@ -62,7 +62,9 @@ def preprocess(sentence):
     and removing any word that does not contain at least one alphabetic
     character.
     """
-    raise NotImplementedError
+    tokens = [word.lower() for word in nltk.word_tokenize(sentence) if word.lower().islower()]
+
+    return tokens
 
 
 def np_chunk(tree):
